b2e4cd350d0002c95936393848c84d9508f82647
Kevin Taing <ktaing789@gmail.com>
2023-04-05 18:04:27 -0400

    updated symbols

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ function getRandomNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
 }
 function getRandomSymbol() {
-  var symbols = '!@#$%^&*(){}[]=<>/,.';
+  var symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
